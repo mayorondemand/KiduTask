49865354727196ddaaf45d119085782af3f7c60a
Prince Eze <pchidireeze@gmail.com>
2025-08-26 13:32:19 +0100

    chore(config): allow google profile domains

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       new URL("https://ik.imagekit.io/bluconvalley/kuditask/**"),
-      new URL("https://lh3.googleusercontent.com"),
+      new URL("https://lh3.googleusercontent.com/**"),
     ],
   },
   eslint: {
